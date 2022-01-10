7cce847d49d209536d78a67b52ac0f02ba167638
Ezyran <claude.schlumberger@gmail.com>
2022-01-10 23:00:23 +0100

    Ajout modèle Client

---
+++ Frontend/src/app/Modules/client/Models/client.ts
@@ -0,0 +1,25 @@
+export class Client
+{   
+    username: string;
+    email: string;
+    name: string;
+    surname: string;
+    telephone: string;
+    zipcode: string;
+    country: string;
+    city: string;
+    address: string;
+
+    constructor()
+    {
+        this.username = "";
+        this.email = "";
+        this.name = "";
+        this.surname = "";
+        this.telephone = "";
+        this.zipcode = "";
+        this.country = "";
+        this.city = "";
+        this.address = "";
+    }
+}
