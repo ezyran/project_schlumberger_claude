447d3ff7933ff5b34d082ac579220e56ea02968f
Ezyran <claude.schlumberger@gmail.com>
2022-01-10 20:39:04 +0100

    Ajout du composant NavBar à la racine de l'application

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
+
+// Components
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/home/home.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
