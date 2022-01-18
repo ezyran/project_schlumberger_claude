5a522ce6e0000b215e7b29eb72c8d48c0b6fb02f
Ezyran <claude.schlumberger@gmail.com>
2022-01-18 18:57:32 +0100

    Backend : Ajustement du contrôleur d'Account

--- Backend/controllers/AccountController.php
+++ Backend/controllers/AccountController.php
@@ -3,6 +3,9 @@
 namespace Application\Controllers;
 
 use Account;
+use Client;
+use Application\Controllers\ClientController;
+
 use Doctrine\ORM\EntityManager;
 
 require_once "./bootstrap.php";
@@ -35,27 +38,37 @@ class AccountController
     public function CreateAccount(array $pAccountInfos)
     {
         // Si 'email' ou 'passwordHash' vide, renvoyer erreur
-        if (!$pAccountInfos["email"] || !$pAccountInfos["passwordHash"])
+        if (!$pAccountInfos["email"] || !$pAccountInfos["passwordHash"] || !$pAccountInfos["clientId"])
             return array("status" => "error", "msg" => "Invalid email or passwordHash");
 
         // On vérifie si l'email n'est déjà enregistrée
         $accountRepository = $this->mEntityManager->getRepository('Account');
-        $account = $accountRepository->findOneBy(array('email' => $pAccountInfos['email']));
+        $account = $accountRepository->findOneByEmail($pAccountInfos['email']);
         if (!is_null($account))
             return array("status" => "error", "msg" => "E-mail already registered");
-        
+
+        // Récupération du Client
+        $clientGetRes = ClientController::GetClientController($this->mEntityManager)->getClient(array( "id" => $pAccountInfos["clientId"]));
+
+        if ($clientGetRes["status"] == "error")
+            return array("status" => "error", "msg" => "GetClient failed on AccountCreation");
+
+        $client = $clientGetRes["msg"];
+
         $newAccount = new Account();
         $newAccount->setEmail($pAccountInfos["email"]);
         $newAccount->setPasswordhash($pAccountInfos["passwordHash"]);
+        $newAccount->setClient($client);
 
         $this->mEntityManager->persist($newAccount);
         $this->mEntityManager->flush();
+
+        return array("status" => "ok", "msg" => $newAccount);
     }
 
     public function AuthenticateAccount(array $pAccountInfos)
     {
         $accountRepository = $this->mEntityManager->getRepository('Account');
-        $accounts = $accountRepository->findAll();
         $account = $accountRepository->findOneByEmail($pAccountInfos['email']);
 
         if (is_null($account))
